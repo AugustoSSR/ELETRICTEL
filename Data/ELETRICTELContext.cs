32e774d004bef2c7cc293255e7797dd54b5fe205
Augusto <augusto.srrosa@gmail.com>
2022-09-27 15:16:08 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ELETRICTELContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ELETRICTEL.Models;
+
+namespace ELETRICTEL.Data
+{
+    public class ELETRICTELContext : DbContext
+    {
+        public ELETRICTELContext (DbContextOptions<ELETRICTELContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ELETRICTEL.Models.Projects> Projects { get; set; } = default!;
+    }
+}
---
+++ Models/Projects.cs
@@ -0,0 +1,8 @@
+﻿namespace ELETRICTEL.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
