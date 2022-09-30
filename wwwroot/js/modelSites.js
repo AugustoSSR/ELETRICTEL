ace2e75f57e62d329331af5c281ffa9d2c1c0696
Augusto Santos <enderdever@gmail.com>
2022-09-30 07:55:25 -0300

    k

---
+++ wwwroot/js/modelSites.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var modelTest = $('modelTest');
+    $('button[data-bs-toggle="modalTypes"]').click(function (event) {
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            modelTest.html(data);
+            modelTest.find('.modal').modal('show');
+        })
+    })
+}) 
